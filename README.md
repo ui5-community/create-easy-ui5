eb186734e5970de3f09bb8b420c0211f1d2ad331
Peter Muessig <peter.muessig@sap.com>
2022-02-20 14:31:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# npm-init wrapper for Easy-UI5
+
+[![License Status][license-image]][license-url]
+
+This NPM package wraps Easy-UI5 in a npm-init or binary.
+
+Via npm init command:
+
+```sh
+npm init easy-ui5
+```
+
+or as binary:
+
+```sh
+easy-ui5
+```
+
+## Support
+
+Please use the GitHub bug tracking system to post questions, bug reports or to create pull requests.
+
+## Contributing
+
+We welcome any type of contribution (code contributions, pull requests, issues) to this generator equally.
+
+## License
+
+This project is licensed under the Apache Software License, version 2.0 except as noted otherwise in the LICENSE file.
+
+[license-image]: https://img.shields.io/github/license/ui5-community/create-easy-ui5.svg
+[license-url]: https://github.com/ui5-community/create-easy-ui5/blob/main/LICENSE
---
+++ index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+// extract the arguments after the first occurrence of easy-ui5
+let include = false;
+const args = process.argv.reduce((currentValue, element) => {
+  if (include) {
+    currentValue.push(element);
+  } else {
+    if (element.includes("easy-ui5")) {
+      include = true;
+    }
+  }
+  return currentValue;
+}, []);
+
+// create the env for the plugin generator
+const yeoman = require("yeoman-environment");
+const env = yeoman.createEnv(args, {
+  cwd: process.cwd(),
+});
+
+// lookup the easy-ui5 generator
+const generators = env
+  .lookup({
+    localOnly: true,
+  })
+  .filter((sub) => {
+    return sub.namespace === "easy-ui5:app";
+  });
+
+// finally, run the subgenerator
+env.run([generators[0].namespace].concat(args), {
+  // verbose: this.options.verbose,
+  // embedded: true,
+});
